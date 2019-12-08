9509f48f68395f69947229b259b75c71f094972a
zman <manzhiyuan@chehejia.com>
2019-12-08 15:28:29 +0800

    add pull functions for IDuplex

--- src/main/java/com/zman/pull/stream/util/Pull.java
+++ src/main/java/com/zman/pull/stream/util/Pull.java
@@ -11,8 +11,32 @@ public class Pull {
         sink.read(source);
     }
 
+    public static <T> void pull(IDuplex<T> a, IDuplex<T> b){
+        pull(a.source(), b.sink());
+    }
+
+    public static <T> void pull(IDuplex<T> a, ISink<T> b){
+        pull(a.source(), b);
+    }
+
+    public static <T> void pull(ISource<T> a, IDuplex<T> b){
+        pull(a, b.sink());
+    }
+
     public static <T, R> void pull(ISource<T> source, IThrough<T, R> through, ISink<R> sink){
         sink.read(through.through(source));
+    }
+
+    public static <T, R> void pull(IDuplex<T> a, IThrough<T, R> through, IDuplex<R> b){
+        pull(a.source(), through, b.sink());
+    }
+
+    public static <T, R> void pull(ISource<T> a, IThrough<T, R> through, IDuplex<R> b){
+        pull(a, through, b.sink());
+    }
+
+    public static <T, R> void pull(IDuplex<T> a, IThrough<T, R> through, ISink<R> b){
+        pull(a.source(), through, b);
     }
 
     public static <T,R> ISource<R> pull(ISource<T> source, IThrough... throughs){
@@ -23,6 +47,9 @@ public class Pull {
         return intermediate;
     }
 
+    public static <T,R> ISource<R> pull(IDuplex<T> duplex, IThrough... throughs){
+        return pull(duplex.source(), throughs);
+    }
 
     /**
      * build two stream flow: source =》 duplex.sink
@@ -37,13 +64,9 @@ public class Pull {
         pull(duplex.source(), sink);
     }
 
-    public static <T> void pull(IDuplex<T> a, IDuplex<T> b){
-        pull(a.source(), b.sink());
-    }
 
-    public static <T, R> void pull(IDuplex<T> source, IThrough<T, R> through, IDuplex<R> sink){
-        sink.sink().read(through.through(source.source()));
-    }
+
+
 
     public static <T> void link(IDuplex<T> source, IDuplex<T> sink){
         pull(source.source(), sink.sink());
